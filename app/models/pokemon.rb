e8ad3460342fb861701289c7c06eba4237fd7125
Chahinez Haddad <mackychahinez@gmail.com>
2022-08-23 15:21:54 +0200

    all seeds done

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,9 +3,8 @@ class Pokemon < ApplicationRecord
 
   validates :name, presence: true
   validates :level, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :type, presence: true
+  validates :pokemon_type, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :purchased, exclusion: [nil]
   validates :picture, presence: true
 end
--- db/migrate/20220823125915_change_column_name_to_pokemon.rb
+++ db/migrate/20220823125915_change_column_name_to_pokemon.rb
@@ -1,4 +1,4 @@
-class ChangeTypeInPokemons < ActiveRecord::Migration[7.0]
+class ChangeColumnNameToPokemon < ActiveRecord::Migration[7.0]
   def change
     rename_column :pokemons, :type, :pokemon_type
   end
