32cbac0cbed3fa885bf2559aa0d43eb62b5b712b
Olivia Bonnal <oliviabonnal@gmail.com>
2022-08-23 15:12:55 +0200

    added create and booking_params methods in BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,10 +5,24 @@ class BookingsController < ApplicationController
   def show
   end
 
-  def confirm
+  def create
+    @user = current_user
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @booking = Booking.new(booking_params)
+    @booking.pokemon = @pokemon
+    @booking.user = @user
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render "pokemons/:id/show", status: :unprocessable_entity
+    end
   end
 
   private
+
+  def booking_params
+    params.require(:booking).permit(:confirmed, :user_id, :pokemon_id)
+  end
 
   def set_user
   end
