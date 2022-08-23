525037d4f553bc7b90b726d49e60acd2b14f0806
Chahinez Haddad <mackychahinez@gmail.com>
2022-08-23 12:11:52 +0200

    Creating basis

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,18 @@
+class BookingsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def confirm
+  end
+
+  private
+
+  def set_user
+  end
+
+  def user_params
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_many :bookings
+  has_many :pokemons, through: :booking
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :admin, inclusion: [true, false] # Comment empecher quelqu'un de changer ce field?
+  validates :balance, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
