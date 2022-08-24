19c3e8b56ddacfdf952968a973f724f0d11cd915
Chahinez Haddad <mackychahinez@gmail.com>
2022-08-24 16:53:32 +0200

    took out other issues

--- app/jobs/destroy_booking_job.rb
+++
@@ -1,8 +0,0 @@
-# class DestroyBookingJob < ApplicationJob
-#   queue_as :default
-
-#   def perform(booking)
-#     # Do something later
-#     booking.destroy unless booking.confirmed
-#   end
-# end
