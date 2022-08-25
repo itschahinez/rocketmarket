bf613c808c3233e7189938cc01efbcf66c012a65
Olivia Bonnal <oliviabonnal@gmail.com>
2022-08-25 10:43:52 +0200

    resolving issues on the dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,9 @@ class PagesController < ApplicationController
     @my_bookings = Booking.where(user: current_user)
     @my_current_orders = @my_bookings.reject { |booking| booking.confirmed }
     @my_past_orders = @my_bookings.select { |booking| booking.confirmed }
-    # @confirmation_status = @my_bookings.map { |booking| booking.confirmed }
     pokemon_ids = @my_current_orders.map { |booking| booking.pokemon_id }
     pokemon_prices = pokemon_ids.map { |id| Pokemon.find(id).price.to_i }
     @bookings_sum = pokemon_prices.sum
     @my_balance = User.find(current_user.id).balance
-    # A voir pour comment on va empêcher un utilisateur d'acheter un Pokémon déjà acheté
-    # @bought_by_someone_else = Booking.all.select { |booking| booking.confirmed }
-    # Ou : @my_current_orders.where(confirmed: true)
   end
 end
