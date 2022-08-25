3b0dd3ff73181de2903c1351737efda3888b9db0
Olivia Bonnal <oliviabonnal@gmail.com>
2022-08-25 15:18:21 +0200

    Added balance update logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
 
   def update_balance
     @user = User.find(params[:user_id])
-    @user.update(user_params)
-    raise
+    @user.update(balance: user_params[:balance].to_i + @user.balance)
     redirect_to dashboard_path
   end
 
