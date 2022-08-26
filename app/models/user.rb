1f94e2376f8fde03b13301ec6ffb11e19a7bd7d9
Chahinez Haddad <mackychahinez@gmail.com>
2022-08-26 11:53:52 +0200

    did some validations, more left to do

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :username, presence: true, uniqueness: true
-  # validates :balance, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
+  validates :username, presence: true, uniqueness: true
+  validates :balance, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
